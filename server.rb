a6778ed1b43c9770a5b59ecabc1a0583a066bd6e
Nikolay Rys <nikolay@rys.me>
2020-10-04 13:36:41 +0200

    Add endpoints boilerplate

--- server.rb
+++ server.rb
@@ -1,7 +1,30 @@
 require 'sinatra'
 require 'dotenv/load'
 
+FIXER_API_URL = 'http://data.fixer.io/api/'
 
 get '/' do
-  'Hello world!'
+  url = "#{request.base_url}/"
+  "Mimics <a href=\"https://fixer.io/documentation\">Fixer API</a>.
+  Supported endpoints:<br>#{url}latest<br>#{url}YYYY-MM-DD<br>#{url}convert<br>#{url}timeseries<br>#{url}fluctuation"
+end
+
+get '/latest' do
+
+end
+
+get '/YYYY-MM-DD' do
+
+end
+
+get '/convert' do
+
+end
+
+get '/timeseries' do
+
+end
+
+get '/fluctuation' do
+
 end
